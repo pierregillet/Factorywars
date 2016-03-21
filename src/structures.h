777fcaafe8f533edef933d24416376b0ad91046d
Pierre Gillet <pierre.gillet@linuxw.info>
2016-03-21 13:58:20 +0100

    added the structures.h file, so that we acan just import this

---
+++ src/structures.h
@@ -0,0 +1,34 @@
+/**
+ * @file
+ * @author Corentin Bocquillon <0x539@nybble.fr>
+ *
+ * @section LICENSE
+ *
+ * Copyright (C) 2016 Corentin Bocquillon
+ * Copyright (C) 2016 Corentin Bocquillon
+ * Copyright (C) 2016 Loup Fourment
+ *
+ * factorywars is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * factorywars is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with factorywars.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ *
+ * structures.h the header file containing most of the structures
+ */
+
+
+struct coordinates
+{
+  long x;
+  long y;
+};
