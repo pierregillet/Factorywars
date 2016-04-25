4955995545bcc5829f3357570865069dd4dbf49d
Loup Fourment <loupfourment@gmail.com>
2016-04-25 14:57:54 +0200

    adding my GUI code and 2 textures

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,10 +1,16 @@
 /**
  * @file
  * @author Corentin Bocquillon <0x539@nybble.fr>
+ * @author Pierre Gillet
+ * @author Loup Fourment
  *
  * @section LICENSE
  *
- * Copyright (C) 2016 Corentin Bocquillon <0x539@nybble.fr>
+ * Copyright (C) 2016 Corentin Bocquillon <corentin@nybble.fr>
+ *
+ * Copyright (C) 2016 Loup Fourment
+ *
+ * Copyright (C) 2016 Pierre Gillet
  *
  * factorywars is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,141 +27,163 @@
  *
  * @section DESCRIPTION
  *
- * gui.cpp is the code for the gui.
+ * gui.cpp contains the gui
  */
 
 #include "gui.h"
-
-int
-init_SDL (SDL_Window** window, SDL_Renderer** renderer)
-{
-  if (SDL_Init (SDL_INIT_VIDEO) < 0)
-    {
-      fprintf (stderr, "SDL could not initialize! SDL Error: %s\n", SDL_GetError ());
-      return 0;
-    }
-
-  //Create window
-  *window = SDL_CreateWindow ("factorywars", SDL_WINDOWPOS_UNDEFINED,
-			      SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-			      SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-  if (*window == NULL)
-    {
-      fprintf(stderr, "Window could not be created! SDL Error: %s\n", SDL_GetError());
-      return 0;
-    }
  
-  *renderer = SDL_CreateRenderer (*window, -1, SDL_RENDERER_ACCELERATED);
-  if (*renderer == NULL)
-    {
-      fprintf(stderr, "Renderer could not be created! SDL Error: %s\n", SDL_GetError());
-      return 0;
-    }
-  SDL_SetRenderDrawColor (*renderer, 0xFF, 0xFF, 0xFF, 0xFF);
- 
-  //Initialize PNG loading
-  int img_flags = IMG_INIT_PNG;
-  if (!(IMG_Init (img_flags) & img_flags))
-    {
-      fprintf(stderr, "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError ());
-      return 0;
-    }
+enum KeyPressTexture
+{
+  KEY_PRESS_SURFACE_DEFAULT, 
+  KEY_PRESS_SURFACE_UP, 
+  KEY_PRESS_SURFACE_DOWN,        
+  KEY_PRESS_SURFACE_LEFT, 
+  KEY_PRESS_SURFACE_RIGHT, 
+  KEY_PRESS_SURFACE_TOTAL
+};
+SDL_Texture* KeyPressTexture [KEY_PRESS_SURFACE_TOTAL];
 
-  return 1;
+SDL_Texture* loadTexture(std::string path, SDL_Renderer* gRenderer)
+{
+  SDL_Texture* NewTexture = NULL;
+  printf("%s\n", path.c_str());
+  SDL_Surface* loadedSurface = IMG_Load(path.c_str() );
+  NewTexture = SDL_CreateTextureFromSurface (gRenderer , loadedSurface);
+  SDL_FreeSurface (loadedSurface);
+  return NewTexture;
 }
 
-int
-load_media (SDL_Renderer** renderer, SDL_Texture** texture)
+bool 
+init (SDL_Window** Window, SDL_Renderer** gRenderer)
 {
-  *texture = load_texture ("media/factorywars.png", renderer);
-  if (*texture == NULL)
-    {
-      fprintf(stderr, "Failed to load PNG image!\n");
-      return 0;
-    }
-
-  return 1;
-}
-
-SDL_Texture*
-load_texture (char* path, SDL_Renderer** renderer)
-{
-  SDL_Texture* new_texture = NULL;
-
-  SDL_Surface* loaded_surface = IMG_Load (path);
-  if (loaded_surface == NULL)
-    {
-      fprintf (stderr, "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError () );
-    }
-  else
-    {
-      new_texture = SDL_CreateTextureFromSurface (*renderer, loaded_surface);
-      if (new_texture == NULL)
-  	{
-  	  fprintf (stderr, "Unable to optimize image %s! SDL Error: %s\n", path, SDL_GetError ());
-  	}
-      SDL_FreeSurface (loaded_surface);
-    }
-
-  return new_texture;
-}
-
-void
-gui ()
-{
-  SDL_Window *window = NULL;
-  SDL_Texture *PNG_texture = NULL;
-  SDL_Renderer *renderer = NULL;
-
-  if (!init_SDL (&window, &renderer))
-    exit (EXIT_FAILURE);
-  if (!load_media (&renderer, &PNG_texture))
-    exit (EXIT_FAILURE);
-
-  // Clear screen
-  SDL_RenderClear (renderer);
-
-  // Render texture to screen
-  SDL_RenderCopy (renderer, PNG_texture, NULL, NULL);
-
-  // Update screen
-  SDL_RenderPresent (renderer);
-
-  // Event Loop
-  int quit = 0;
-  SDL_Event e;
-
-  while (!quit)
-    {
-      while (SDL_PollEvent (&e))
+	bool success = true;
+	
+	if (SDL_Init (SDL_INIT_VIDEO) < 0)
 	{
-	  switch (e.type)
-	    {
-	    case SDL_QUIT:
-	      quit = 1;
-	      break;
-	    }
+		printf ("\n erreur: ", SDL_GetError ());
+		success = false;
 	}
-    }
-
-  exit_SDL (&PNG_texture, &renderer, &window);
+	
+	else //si la SDL s'est bien lancee	
+	{
+	  SDL_Window* Window = NULL;
+	  Window = SDL_CreateWindow ("TEST",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,480, SDL_WINDOW_SHOWN);
+	  
+	  if (Window == NULL) 
+	  {
+	    success = false;
+	    printf (SDL_GetError());
+	  }
+	  
+	  else //si la fenetre est bien cree
+	  {
+	    SDL_Renderer* gRenderer = NULL;
+	    gRenderer = SDL_CreateRenderer (Window, -1, SDL_RENDERER_ACCELERATED);
+	    SDL_SetRenderDrawColor (gRenderer, 0xFF,0xFF,0xFF,0xFF);
+	  }
+	  
+  }
+  return success;
 }
 
-void
-exit_SDL (SDL_Texture** texture, SDL_Renderer** renderer, SDL_Window** window)
+
+bool 
+loadMedia (SDL_Texture** KeyPressTexture, SDL_Renderer* gRenderer)
 {
-  SDL_DestroyTexture (*texture);
-  *texture = NULL;
-
-  // Destroy texture
-  SDL_DestroyRenderer (*renderer);
-  *renderer = NULL;
-
-  // Destroy window
-  SDL_DestroyWindow (*window);
-  *window = NULL;
-  
-  //Quit SDL subsystems
-  IMG_Quit();
-  SDL_Quit();
+  bool success = true; 
+  //chaque case du tableau se voit atribuer une image
+  KeyPressTexture[ KEY_PRESS_SURFACE_DEFAULT ] = loadTexture ("Hello_world.png", gRenderer); 
+  if (KeyPressTexture[KEY_PRESS_SURFACE_DEFAULT ] == NULL)
+    success = false;
+    
+  KeyPressTexture[KEY_PRESS_SURFACE_UP ] = loadTexture ("up.png", gRenderer);
+  if (KeyPressTexture[KEY_PRESS_SURFACE_UP ] == NULL)
+    success = false;
+    
+  KeyPressTexture[KEY_PRESS_SURFACE_DOWN ] = loadTexture ("down.png", gRenderer);
+  if (KeyPressTexture[KEY_PRESS_SURFACE_DOWN ] == NULL)
+    success = false;
+    
+  KeyPressTexture[KEY_PRESS_SURFACE_LEFT ] = loadTexture ("left.png", gRenderer);
+  if (KeyPressTexture[KEY_PRESS_SURFACE_LEFT ] == NULL)
+    success = false;  
+    
+  KeyPressTexture[KEY_PRESS_SURFACE_RIGHT ] = loadTexture ("right.png", gRenderer);
+  if (KeyPressTexture[KEY_PRESS_SURFACE_DEFAULT ] == NULL)
+    success = false;
+    
+  return success;
 }
+
+
+bool
+blit (int x,int y, SDL_Texture* texture, SDL_Renderer* gRenderer)
+{
+  bool success=true;
+  SDL_Rect Rect;
+  Rect.x = x;
+  Rect.y = y;
+  Rect.w = 640;
+  Rect.h = 480;
+  SDL_RenderSetViewport(gRenderer, &Rect);
+  SDL_RenderClear (gRenderer);
+  SDL_RenderCopy (gRenderer, texture, NULL,NULL);
+  SDL_RenderPresent (gRenderer);
+  return success;
+}
+
+int 
+run_gui ()
+{
+  SDL_Window* Window = NULL;
+  SDL_Renderer* gRenderer = NULL;
+  if (!init (&Window, &gRenderer))
+    return 1;
+  
+  int x = 0;
+  int y = 0;
+  bool quit = false;
+  
+  SDL_Event e;
+  SDL_Texture* CurrentTexture = NULL;
+  CurrentTexture = KeyPressTexture [ KEY_PRESS_SURFACE_DEFAULT ];
+  while ( !quit )
+  {
+    while (SDL_PollEvent(&e) != 0)
+    {
+      if (e.type == SDL_QUIT)
+        quit = true;
+      else if (e.type == SDL_KEYDOWN)
+      {
+        switch (e.key.keysym.sym)
+        {
+          case SDLK_UP:
+          y--;
+          CurrentTexture= KeyPressTexture[KEY_PRESS_SURFACE_UP];
+          break;
+         
+          case SDLK_DOWN:
+          y++;
+          CurrentTexture= KeyPressTexture[KEY_PRESS_SURFACE_DOWN];
+          break;
+            
+          case SDLK_LEFT:
+          x--;
+          CurrentTexture= KeyPressTexture[KEY_PRESS_SURFACE_LEFT];
+          break;
+            
+          case SDLK_RIGHT:
+          x++; 
+          CurrentTexture= KeyPressTexture[KEY_PRESS_SURFACE_RIGHT];
+          break;
+            
+          default:  
+          CurrentTexture= KeyPressTexture[KEY_PRESS_SURFACE_DEFAULT];
+          break;
+        }            
+      }
+      blit(x, y, CurrentTexture, gRenderer);    
+    }         
+  } 
+}  
+
--- src/gui.h
+++ src/gui.h
@@ -6,7 +6,7 @@
  *
  * @section LICENSE
  *
- * Copyright (C) 2016 Corentin Bocquillon <0x539@nybble.fr>
+ * Copyright (C) 2016 Corentin Bocquillon <corentin@nybble.fr>
  *
  * Copyright (C) 2016 Loup Fourment
  *
@@ -23,44 +23,31 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with factorywars.  If not, see <http://www.gnu.org/licenses/
+ * along with factorywars.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @section DESCRIPTION
  *
  * gui.h is the header of gui.cpp
  */
 
-#pragma once
-
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
-#include <unistd.h>
-
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+#include <string>
 
 /**
- * Initialize the SDL and SDL_image libraries.
- */
-int init_SDL (SDL_Window** window, SDL_Texture** renderer);
+  * Blit Textures at given coordonates x,y
+  * Returns a bool named success, true or false
+  * The Texture needs a Renderer for being blited
+  */
+bool blit (int x, int y, SDL_Texture*, SDL_Renderer* gRenderer);
 
 /**
- * Load the textures.
- */
-int load_media (SDL_Renderer** renderer, SDL_Texture** texture);
+  * Initiates the SDL basics, like Window and Renderer
+  */
+bool init (SDL_Window** Window, SDL_Renderer** gRenderer);
+bool loadMedia (SDL_Texture** KeyPressTexture, SDL_Renderer* gRenderer);  
+void close ();                //fermeture
+int run_gui ();
+SDL_Texture* loadTexture (std::string path, SDL_Renderer* gRenderer);
 
-/**
- * Load PNG file.
- */
-SDL_Texture* load_texture (char* path, SDL_Renderer** renderer);
-
-/**
- * Run the gui.
- */
-void gui ();
-
-/**
- * Exit the SDL and SDL_image libraries.
- */
-void exit_SDL (SDL_Texture** texture, SDL_Renderer** renderer, SDL_Window** window);
