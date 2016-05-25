873ad0dfd4f5015b3b25b049cba9299c4db877f6
Loup Fourment <loupfourment@gmail.com>
2016-05-25 14:26:28 +0200

    display items on good coords

--- src/display_item.cpp
+++ src/display_item.cpp
@@ -24,8 +24,8 @@ display_items (SDL_Renderer** Renderer, std::string path, SDL_Texture** table_it
 	  if (item_id > 0 && item_id < 4)
 	    {
 	      struct coordinates bliting_coords;
-	      bliting_coords.x = (m - screen_origin.x % 384) + 24*square_coords.x;
-	      bliting_coords.y = (l - screen_origin.y % 384) + 24*square_coords.y;
+	      bliting_coords.x = (m - screen_origin.x % 384) + 24*square_coords.y;
+	      bliting_coords.y = (l - screen_origin.y % 384) + 24*square_coords.x;
 	      
 	      SDL_Texture* item_texture = table_items[item_id];
 	      blit(Renderer, bliting_coords, 24, 24, item_texture);
