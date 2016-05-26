47fb0074b5018feed4d06c96dc0e75d5b5eeb5ff
Pierre Gillet <pierre.gillet+framagit@linuxw.info>
2016-05-26 21:31:15 +0200

    Changed the items bliting coordinates so that we click on the bottom of the trees instead of their top.

--- src/display_item.cpp
+++ src/display_item.cpp
@@ -29,12 +29,13 @@ display_items (SDL_Renderer** Renderer,
 	  int item_id = chunk.squares[i][j];
 	  if (item_id > 0 && item_id < 4)
 	    {
-	      struct coordinates bliting_coords;
-	      bliting_coords.x = (m - screen_origin.x % 384) + 24*i;
-	      bliting_coords.y = (l - screen_origin.y % 384) + 24*j;
-	      
 	      int blit_x = (item_id == 1)? 48 :24;
 	      int blit_y = (item_id == 1)? 48 :24;
+	      struct coordinates bliting_coords;
+	      
+	      bliting_coords.x = (m - screen_origin.x % 384) + 24 * i - blit_x / 4;
+	      bliting_coords.y = (l - screen_origin.y % 384) + 24 * j - blit_y / 2;
+	      
 	      SDL_Texture* item_texture = table_items[item_id];
 	      blit(Renderer, bliting_coords, blit_x, blit_y, item_texture);
 	    }
